7d9bf71c7122a4b2e153ff5d7924e028f23981e7
Tanvi Penumudy <46785609+tanvipenumudy@users.noreply.github.com>
2020-11-04 21:07:49 +0530

    Added voice input - 2

---
+++ Voice Input Test-2/Chrome-Web-Speech-API/js/languages.js
@@ -0,0 +1,13 @@
+var langs =
+['English',        ['en-IN', 'India'],
+                   ['en-AU', 'Australia'],
+                     ['en-CA', 'Canada'],
+                     ['en-KE', 'Kenya'],
+                     ['en-TZ', 'Tanzania'],
+                     ['en-GH', 'Ghana'],
+                     ['en-NZ', 'New Zealand'],
+                     ['en-NG', 'Nigeria'],
+                     ['en-ZA', 'South Africa'],
+                     ['en-PH', 'Philippines'],
+                     ['en-GB', 'United Kingdom'],
+                     ['en-US', 'United States']];
