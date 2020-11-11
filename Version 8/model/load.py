070b82289ddf9a984e7e97bb82ee317a46b62d30
Tanvi Penumudy <46785609+tanvipenumudy@users.noreply.github.com>
2020-11-11 18:18:15 +0530

    Added ver-8.0 'Final'

---
+++ Version 8/model/load.py
@@ -0,0 +1,23 @@
+from keras.models import model_from_json
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+import os
+app_root = os.path.dirname(os.path.abspath(__file__))
+def init():
+    target1 = os.path.join(app_root, 'schonell.json')
+    target2 = os.path.join(app_root, 'schonell.h5')
+    target3 = os.path.join(app_root, 'schonell1.json')
+    target4 = os.path.join(app_root, 'schonell1.h5')
+    json_file = open(target1, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    loaded_model.load_weights(target2)
+    loaded_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    json_file = open(target3, 'r')
+    loaded_model1_json = json_file.read()
+    json_file.close()
+    loaded_model1 = model_from_json(loaded_model1_json)
+    loaded_model1.load_weights(target4)
+    loaded_model1.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return loaded_model, loaded_model1
